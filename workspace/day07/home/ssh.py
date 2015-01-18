300ee5aae60df7d0442fd75926219f118abab6df
Mingming Cong <mingming_cong@foxitsoftware.com>
2015-01-18 05:47:09 -0500

    day0701

--- workspace/day07/home/ssh.py
+++ workspace/day07/home/ssh.py
@@ -1,25 +1,33 @@
 #!/user/bin/env python
 #coding:utf-8
 import paramiko
+class ssh_ser():
 
-def ssh_ser(ser_host,ser_cmd):
-    paramiko.util.log_to_file('/tmp/redmine.log')
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ser_host,22,'root','kevinstar')
-    stdin,stdout,stderr = ssh.exec_command(ser_cmd)
-    print stdout.read()
-    ssh.close()
+    def __init__(self,Host,Username,Passwdi,Cmd):
+        self.Host=host
+        self.Port=port
+        self.Username=username
+        self.Passwd=password
+        self.Cmd=cmd
+         
+    def ssh_ser(self):
+        paramiko.util.log_to_file('/tmp/redmine.log')
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(self.Host,sefl.Port,self.Username,self.Cmd)
+        stdin,stdout,stderr = ssh.exec_command(ser_cmd)
+        print stdout.read()
+        ssh.close()
 
-def ssh_put():
-    sftp = paramiko.Transport(('ip',22))
+    def ssh_put(self):
+        sftp = paramiko.Transport((self.Host,self.Port))
 
-def first_conn():
-    cmd_base=('echo VM06 > /etc/hostname','apt-get -y update && apt-get dist-upgrade','apt-get -y install ntp')
-    for cmd in cmd_base:
-        ssh_ser('192.168.1.91',cmd)
+    def first_conn(self):
+        cmd_base=('echo VM06 > /etc/hostname','apt-get -y update && apt-get dist-upgrade','apt-get -y install ntp')
+        for cmd in self.Cmd:
+            ssh_ser(self.Port,cmd)
 
-def inst_redmine(cmd_poll):
-    for cmd in cmd_poll:
-        ssh_ser('192.168.1.91',cmd.strip())
-        #print cmd.strip()
+    def inst_redmine(self):
+        for cmd in self.Cmd:
+            ssh_ser('192.168.1.91',cmd.strip())
+            #print cmd.strip()
